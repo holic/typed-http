04ce3af05f22a2feb15454503116537bb5aab436
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-20 13:05:19 +0100

    works, but is it good enough?

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,6 +1,8 @@
-import type { inferAmbient, Type } from "arktype";
+import type { conform } from "@ark/util";
+import type { inferAmbient, Type, validateAmbient } from "arktype";
 
 export type Shape<shape> = {
+  shape: shape;
   is(value: unknown): value is shape;
   validate(value: unknown): shape | Error;
 };
@@ -8,29 +10,54 @@ export type Shape<shape> = {
 export type Pipe<input, output, pipeOutput = output | Error> = {
   input: Shape<input>;
   output: Shape<output>;
-  pipe(value: input): pipeOutput;
+  pipe<const value>(value: conform<value, input>): pipeOutput;
 };
 
 // turn arktype def into pipe
-type inferArktypePipe<def, t = inferAmbient<def>> = {
-  input: {
-    is(value: unknown): value is Type<t>["inferIn"];
-    validate(value: unknown): Type<t>["inferIn"] | Error;
-  };
-  output: {
-    is(value: unknown): value is Type<t>["infer"];
-    validate(value: unknown): Type<t>["infer"] | Error;
-  };
-  pipe(value: Type<t>["inferIn"]): Type<t>["infer"] | Error;
-};
+type inferArktypePipe<def, t = inferAmbient<def>> = Pipe<
+  Type<t>["inferIn"],
+  Type<t>["infer"]
+>;
 
 export type Action<
   input extends Pipe<any, any>,
   output extends Pipe<any, any>
-> = Pipe<input, output, Promise<output | Error>>;
+> = {
+  input: input;
+  output: output;
+  pipe<const value>(
+    value: conform<value, input["input"]["shape"]>
+  ): Promise<output["output"]["shape"] | Error>;
+};
 
 // turn arktype input/output defs into an action
 type inferArktypeAction<inputDef, outputDef> = Action<
   inferArktypePipe<inputDef>,
   inferArktypePipe<outputDef>
 >;
+
+function createShape<def>(def: validateAmbient<def>): inferArktypePipe<def> {
+  return {} as never;
+}
+
+function createAction<inputDef, outputDef>(
+  inputDef: validateAmbient<inputDef>,
+  outputDef: validateAmbient<outputDef>
+): inferArktypeAction<inputDef, outputDef> {
+  return {} as never;
+}
+
+const listUsers = createAction(
+  {
+    "limit?": ["string", "=>", (str) => parseInt(str)],
+  },
+  [
+    {
+      id: "number",
+      username: "string",
+    },
+    "[]",
+  ]
+);
+
+listUsers.pipe({ limit: "1" });
