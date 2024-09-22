78df7a6e6f76f88adba5864f839cd63d35d4ce79
Kevin Ingersoll <kingersoll@gmail.com>
2024-09-22 15:04:01 +0100

    fiddling with API designs

---
+++ src/scratch.ts
@@ -0,0 +1,86 @@
+/*
+
+router({
+  user: {
+    byId: route.get("/users:/id")
+      .in({
+        id: "id",
+        "createdAt?": "date",
+      })
+      .out({
+        id: "id",
+        username: "string",
+        createdAt: "date",
+      })
+      .execute(async ({ input }) => {
+        return {
+          id: input.id,
+          username: "alice",
+          createdAt: new Date(2000, 1),
+        };
+      }),
+  },
+});
+
+route
+  .get("/users:/id")
+  .in({
+    id: "id",
+    "createdAt?": "date",
+  })
+  .out({
+    id: "id",
+    username: "string",
+    createdAt: "date",
+  })
+  .execute(async ({ input }) => {
+    return {
+      id: input.id,
+      username: "alice",
+      createdAt: new Date(2000, 1),
+    };
+  });
+
+createRoute({
+  method: "GET",
+  route: "/users/:id",
+  action: createHttpAction({
+    input: {
+      id: "id",
+      "createdAt?": "date",
+    },
+    output: {
+      id: "id",
+      username: "string",
+      createdAt: "date",
+    },
+    async execute({ input }) {
+      return {
+        id: input.id,
+        username: "alice",
+        createdAt: new Date(2000, 1),
+      };
+    },
+  }),
+});
+
+createRoute("GET", "/users/:id", createHttpAction({
+  input: {
+    id: "id",
+    "createdAt?": "date",
+  },
+  output: {
+    id: "id",
+    username: "string",
+    createdAt: "date",
+  },
+  async execute({ input }) {
+    return {
+      id: input.id,
+      username: "alice",
+      createdAt: new Date(2000, 1),
+    };
+  },
+}));
+
+*/
